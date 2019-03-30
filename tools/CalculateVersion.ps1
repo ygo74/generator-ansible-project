0ce106f88c768eb14545d7fdeb6c968ab8f22bef
ygo74 <yannickgobert@yahoo.fr>
2019-03-30 19:29:40 +0100

    Calculate new version

---
+++ tools/CalculateVersion.ps1
@@ -0,0 +1,22 @@
+#https://docs.microsoft.com/en-us/azure/devops/pipelines/scripts/powershell?view=azure-devops
+
+# Enable -Verbose option
+[CmdletBinding()]
+
+#Retrieve Versions in the public npm
+$res = npm show generator-ansible-project@* version
+$foundVersions = @()
+$res | ForEach-Object {
+    $versions = $_ -split " "
+    $foundVersions += New-Object -TypeName System.Version $versions[1].trim().replace("'","")
+}
+
+if ($foundVersions.length -eq 0)
+{
+    $latestVersion = New-Object -TypeName System.Version 0.0.1
+}
+else {
+    $latestVersion = ($foundVersions |Sort-Object -Descending)[0]
+}
+
+write-output "##vso[task.setvariable variable=myOutputVar;isOutput=true]$latestVersion"
